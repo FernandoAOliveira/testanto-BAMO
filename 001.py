31331d622fb579f4af8797e17a95045484c54ae2
Fábio Souza <fabiobarboza7@hotmail.com>
2020-01-29 17:52:42 -0300

    criando mais opções

--- 001.py
+++ 001.py
@@ -5,5 +5,15 @@ nome = input('Qual o seu nome? ')
 dia = input('Qual o dia do seu nascimento? ')
 mês = input('Qual o mês do seu nascimento? ')
 ano = input('Qual o ano do seu nascimento? ')
+
+nome = input('Qual o seu nome? ')
+dia = input('Qual o dia do seu nascimento? ')
+mês = input('Qual o mês do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
+
+nome = input('Qual o seu nome? ')
+dia = input('Qual o dia do seu nascimento? ')
+mês = input('Qual o mês do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
 print('{}, sua data de nascimento é {}/{}/{}!'.format(nome, dia, mês, ano))
 
